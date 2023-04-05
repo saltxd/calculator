3362e3a30606738e9f28f93a88ab9a4b6be9a25b
marley <74526520+saltxd@users.noreply.github.com>
2023-04-05 13:30:41 -0600

    Create operate function that calls on other functions

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 
+let firstNumber = '';
+let operator = '';
+let secondNumber = '';
+
+
 function add(num1, num2){
     return num1 + num2;
 }
@@ -12,5 +17,24 @@ function multiply(num1, num2){
 }
 
 function divide(num1, num2){
+    if (num2 === 0) {
+        return "Cannot divide by zero!";
+    } else {
     return num1 / num2;
+    }
+}
+
+function operate(operator, num1, num2){
+    switch(operator) {
+        case '+':
+            return add(num1, num2);
+        case '-':
+            return subtract(num1, num2);
+        case '*':
+            return multiply(num1, num2);
+        case '/':
+            return divide(num1, num2);
+        default:
+            return 'Error: Invalid operator!'
+    }
 }
